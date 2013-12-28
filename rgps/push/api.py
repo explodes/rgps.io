c1d444a08f6508b4ee3782e428953f43b140ea91
Evan Leis <evan.leis@wearetopsecret.com>
2013-12-28 06:02:08 -0700

    Proximo

--- rgps/push/api.py
+++ rgps/push/api.py
@@ -1,25 +1,43 @@
+import os
 import time
 
+import requests
 from django.conf import settings
-from gcm import GCM
 
 
-gcm = GCM(settings.GCM_API_KEY)
+GCM_ENDPOINT = "https://android.googleapis.com/gcm/send"
 
 
 def send_gps_request_to_user(user):
-    regIds = [user.registration_id]
-    data = {'action': 'gps', 'frequency': settings.GPS_UPDATE_FRQ, 'count': settings.GPS_UPDATE_COUNT}
+    headers = {
+        'Authentication': 'key=%s' % settings.GCM_API_KEY,
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'registration_ids': [user.registration_id],
+        'data': {
+            'action': 'gps',
+            'frequency': settings.GPS_UPDATE_FRQ,
+            'count': settings.GPS_UPDATE_COUNT
+        }
+    }
+
+    if os.environ.get('PROXIMO_URL'):
+        proxies = {'http': os.environ.get('PROXIMO_URL', '')}
+    else:
+        proxies = None
 
     attempt = 1
 
     while attempt < 10:
         try:
-            gcm.json_request(registration_ids=regIds, data=data)
+            response = requests.post(GCM_ENDPOINT, data=data, headers=headers, proxies=proxies)
+            if response.status_code != 200:
+                raise Exception('Back off')
         except:
             if attempt == 10:
                 return None
-            time.sleep(attempt ** 2 * 10)
+            time.sleep(attempt ** 2 * 12)
             attempt += 1
         else:
             return
